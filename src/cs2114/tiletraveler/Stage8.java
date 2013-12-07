ef1dde1fc439dc311402dddc71b842ca50030330
sjacob95 <sjacob95@vt.edu>
2013-12-07 11:59:42 -0500

    Added Sex Appeal to Stage 8
    I need a change of pants, guys.

--- src/cs2114/tiletraveler/Stage8.java
+++ src/cs2114/tiletraveler/Stage8.java
@@ -112,21 +112,25 @@ public class Stage8
         super(map, startLoc);
 
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(3, 34), new Location(
-                18,
-                34)));
+            new Floater(tileSize, this,
+                new Location(3, 34),
+                new Location(18, 34)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(19, 34), new Location(
-                34,
-                34)));
+            new Floater(tileSize, this,
+                new Location(19, 34),
+                new Location(34, 34)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(8, 36), new Location(
-                18,
-                36), new Location(3, 36), new Location(8, 36)));
+            new Floater(tileSize, this,
+                new Location(8, 36),
+                new Location(18, 36),
+                new Location(3, 36),
+                new Location(8, 36)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(24, 36), new Location(
-                34,
-                36), new Location(19, 36), new Location(24, 36)));
+            new Floater(tileSize, this,
+                new Location(24, 36),
+                new Location(34, 36),
+                new Location(19, 36),
+                new Location(24, 36)));
 
         getEnemyMap().addEnemy(
             new Bug(
@@ -166,201 +170,255 @@ public class Stage8
                 new Location(1, 15)));
 
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(3, 33), new Location(
-                34,
-                33)));
+            new Floater(tileSize, this,
+                new Location(3, 33),
+                new Location(34, 33)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(34, 37), new Location(
-                3,
-                37)));
+            new Floater(tileSize, this,
+                new Location(34, 37),
+                new Location(3, 37)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(15, 35), new Location(
-                34,
-                35), new Location(3, 35), new Location(15, 35)));
+            new Floater(tileSize, this,
+                new Location(15, 35),
+                new Location(34, 35),
+                new Location(3, 35),
+                new Location(15, 35)));
 
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(8, 31), new Location(
-                29,
-                31), new Location(29, 30), new Location(8, 30), new Location(
-                8,
-                31)));
+            new Floater(tileSize, this,
+                new Location(8, 31),
+                new Location(29, 31),
+                new Location(29, 30),
+                new Location(8, 30),
+                new Location(8, 31)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(12, 31), new Location(
-                29,
-                31), new Location(29, 30), new Location(8, 30), new Location(
-                8,
-                31), new Location(12, 31)));
+            new Floater(tileSize, this,
+                new Location(12, 31),
+                new Location(29, 31),
+                new Location(29, 30),
+                new Location(8, 30),
+                new Location(8, 31),
+                new Location(12, 31)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(16, 31), new Location(
-                29,
-                31), new Location(29, 30), new Location(8, 30), new Location(
-                8,
-                31), new Location(16, 31)));
+            new Floater(tileSize, this,
+                new Location(16, 31),
+                new Location(29, 31),
+                new Location(29, 30),
+                new Location(8, 30),
+                new Location(8, 31),
+                new Location(16, 31)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(20, 31), new Location(
-                29,
-                31), new Location(29, 30), new Location(8, 30), new Location(
-                8,
-                31), new Location(20, 31)));
+            new Floater(tileSize, this,
+                new Location(20, 31),
+                new Location(29, 31),
+                new Location(29, 30),
+                new Location(8, 30),
+                new Location(8, 31),
+                new Location(20, 31)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(24, 31), new Location(
-                29,
-                31), new Location(29, 30), new Location(8, 30), new Location(
-                8,
-                31), new Location(24, 31)));
+            new Floater(tileSize, this,
+                new Location(24, 31),
+                new Location(29, 31),
+                new Location(29, 30),
+                new Location(8, 30),
+                new Location(8, 31),
+                new Location(24, 31)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(28, 31), new Location(
-                29,
-                31), new Location(29, 30), new Location(8, 30), new Location(
-                8,
-                31), new Location(28, 31)));
+            new Floater(tileSize, this,
+                new Location(28, 31),
+                new Location(29, 31),
+                new Location(29, 30),
+                new Location(8, 30),
+                new Location(8, 31),
+                new Location(28, 31)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(27, 30), new Location(
-                8,
-                30), new Location(8, 31), new Location(29, 31), new Location(
-                29,
-                30), new Location(27, 30)));
+            new Floater(tileSize, this,
+                new Location(27, 30),
+                new Location(8, 30),
+                new Location(8, 31),
+                new Location(29, 31),
+                new Location(29, 30),
+                new Location(27, 30)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(23, 30), new Location(
-                8,
-                30), new Location(8, 31), new Location(29, 31), new Location(
-                29,
-                30), new Location(23, 30)));
+            new Floater(tileSize, this,
+                new Location(23, 30),
+                new Location(8, 30),
+                new Location(8, 31),
+                new Location(29, 31),
+                new Location(29, 30),
+                new Location(23, 30)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(19, 30), new Location(
-                8,
-                30), new Location(8, 31), new Location(29, 31), new Location(
-                29,
-                30), new Location(19, 30)));
+            new Floater(tileSize, this,
+                new Location(19, 30),
+                new Location(8, 30),
+                new Location(8, 31),
+                new Location(29, 31),
+                new Location(29, 30),
+                new Location(19, 30)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(15, 30), new Location(
-                8,
-                30), new Location(8, 31), new Location(29, 31), new Location(
-                29,
-                30), new Location(15, 30)));
+            new Floater(tileSize, this,
+                new Location(15, 30),
+                new Location(8, 30),
+                new Location(8, 31),
+                new Location(29, 31),
+                new Location(29, 30),
+                new Location(15, 30)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(11, 30), new Location(
-                8,
-                30), new Location(8, 31), new Location(29, 31), new Location(
-                29,
-                30), new Location(11, 30)));
+            new Floater(tileSize, this,
+                new Location(11, 30),
+                new Location(8, 30),
+                new Location(8, 31),
+                new Location(29, 31),
+                new Location(29, 30),
+                new Location(11, 30)));
 
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(8, 28), new Location(
-                29,
-                28), new Location(29, 27), new Location(8, 27), new Location(
-                8,
-                28)));
+            new Floater(tileSize, this,
+                new Location(8, 28),
+                new Location(29, 28),
+                new Location(29, 27),
+                new Location(8, 27),
+                new Location(8, 28)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(12, 28), new Location(
-                29,
-                28), new Location(29, 27), new Location(8, 27), new Location(
-                8,
-                28), new Location(12, 28)));
+            new Floater(tileSize, this,
+                new Location(12, 28),
+                new Location(29, 28),
+                new Location(29, 27),
+                new Location(8, 27),
+                new Location(8, 28),
+                new Location(12, 28)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(16, 28), new Location(
-                29,
-                28), new Location(29, 27), new Location(8, 27), new Location(
-                8,
-                28), new Location(16, 28)));
+            new Floater(tileSize, this,
+                new Location(16, 28),
+                new Location(29, 28),
+                new Location(29, 27),
+                new Location(8, 27),
+                new Location(8, 28),
+                new Location(16, 28)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(20, 28), new Location(
-                29,
-                28), new Location(29, 27), new Location(8, 27), new Location(
-                8,
-                28), new Location(20, 28)));
+            new Floater(tileSize, this,
+                new Location(20, 28),
+                new Location(29, 28),
+                new Location(29, 27),
+                new Location(8, 27),
+                new Location(8, 28),
+                new Location(20, 28)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(24, 28), new Location(
-                29,
-                28), new Location(29, 27), new Location(8, 27), new Location(
-                8,
-                28), new Location(24, 28)));
+            new Floater(tileSize, this,
+                new Location(24, 28),
+                new Location(29, 28),
+                new Location(29, 27),
+                new Location(8, 27),
+                new Location(8, 28),
+                new Location(24, 28)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(28, 28), new Location(
-                29,
-                28), new Location(29, 27), new Location(8, 27), new Location(
-                8,
-                28), new Location(28, 28)));
+            new Floater(tileSize, this,
+                new Location(28, 28),
+                new Location(29, 28),
+                new Location(29, 27),
+                new Location(8, 27),
+                new Location(8, 28),
+                new Location(28, 28)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(27, 27), new Location(
-                8,
-                27), new Location(8, 28), new Location(29, 28), new Location(
-                29,
-                27), new Location(27, 27)));
+            new Floater(tileSize, this,
+                new Location(27, 27),
+                new Location(8, 27),
+                new Location(8, 28),
+                new Location(29, 28),
+                new Location(29, 27),
+                new Location(27, 27)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(23, 27), new Location(
-                8,
-                27), new Location(8, 28), new Location(29, 28), new Location(
-                29,
-                27), new Location(23, 27)));
+            new Floater(tileSize, this,
+                new Location(23, 27),
+                new Location(8, 27),
+                new Location(8, 28),
+                new Location(29, 28),
+                new Location(29, 27),
+                new Location(23, 27)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(19, 27), new Location(
-                8,
-                27), new Location(8, 28), new Location(29, 28), new Location(
-                29,
-                27), new Location(19, 27)));
+            new Floater(tileSize, this,
+                new Location(19, 27),
+                new Location(8, 27),
+                new Location(8, 28),
+                new Location(29, 28),
+                new Location(29, 27),
+                new Location(19, 27)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(15, 27), new Location(
-                8,
-                27), new Location(8, 28), new Location(29, 28), new Location(
-                29,
-                27), new Location(15, 27)));
+            new Floater(tileSize, this,
+                new Location(15, 27),
+                new Location(8, 27),
+                new Location(8, 28),
+                new Location(29, 28),
+                new Location(29, 27),
+                new Location(15, 27)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(11, 27), new Location(
-                8,
-                27), new Location(8, 28), new Location(29, 28), new Location(
-                29,
-                27), new Location(11, 27)));
+            new Floater(tileSize, this,
+                new Location(11, 27),
+                new Location(8, 27),
+                new Location(8, 28),
+                new Location(29, 28),
+                new Location(29, 27),
+                new Location(11, 27)));
 
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(25, 22), new Location(
-                12,
-                22), new Location(12, 16), new Location(25, 16), new Location(
-                25,
-                22)));
+            new Floater(tileSize, this,
+                new Location(25, 22),
+                new Location(12, 22),
+                new Location(12, 16),
+                new Location(25, 16),
+                new Location(25, 22)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(12, 22), new Location(
-                12,
-                16), new Location(25, 16), new Location(25, 22), new Location(
-                12,
-                22)));
+            new Floater(tileSize, this,
+                new Location(12, 22),
+                new Location(12, 16),
+                new Location(25, 16),
+                new Location(25, 22),
+                new Location(12, 22)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(12, 16), new Location(
-                25,
-                16), new Location(25, 22), new Location(12, 22), new Location(
-                12,
-                16)));
+            new Floater(tileSize, this,
+                new Location(12, 16),
+                new Location(25, 16),
+                new Location(25, 22),
+                new Location(12, 22),
+                new Location(12, 16)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(25, 16), new Location(
-                25,
-                22), new Location(12, 22), new Location(12, 16), new Location(
-                25,
-                16)));
+            new Floater(tileSize, this,
+                new Location(25, 16),
+                new Location(25, 22),
+                new Location(12, 22),
+                new Location(12, 16),
+                new Location(25, 16)));
 
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(23, 20), new Location(
-                14,
-                20), new Location(14, 18), new Location(23, 18), new Location(
-                23,
-                20)));
+            new Floater(tileSize, this,
+                new Location(23, 20),
+                new Location(14, 20),
+                new Location(14, 18),
+                new Location(23, 18),
+                new Location(23, 20)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(17, 20), new Location(
-                14,
-                20), new Location(14, 18), new Location(23, 18), new Location(
-                23,
-                20), new Location(17, 20)));
+            new Floater(tileSize, this,
+                new Location(17, 20),
+                new Location(14, 20),
+                new Location(14, 18),
+                new Location(23, 18),
+                new Location(23, 20),
+                new Location(17, 20)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(14, 18), new Location(
-                23,
-                18), new Location(23, 20), new Location(14, 20), new Location(
-                14,
-                18)));
+            new Floater(tileSize, this,
+                new Location(14, 18),
+                new Location(23, 18),
+                new Location(23, 20),
+                new Location(14, 20),
+                new Location(14, 18)));
         getEnemyMap().addEnemy(
-            new Floater(tileSize, this, new Location(20, 18), new Location(
-                23,
-                18), new Location(23, 20), new Location(14, 20), new Location(
-                14,
-                18), new Location(20, 18)));
+            new Floater(tileSize, this,
+                new Location(20, 18),
+                new Location(23, 18),
+                new Location(23, 20),
+                new Location(14, 20),
+                new Location(14, 18),
+                new Location(20, 18)));
 
     }
 
