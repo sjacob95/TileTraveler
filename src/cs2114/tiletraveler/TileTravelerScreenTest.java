4abbb2fcfd42257808a98dc6584dd246c24205ed
mrzchuck <MrZchuck@Ezra-Richardss-MacBook-Pro-4.local>
2013-12-06 23:03:40 -0500

    Changes to the menu item clicks in the TileTraveler Screen.

--- src/cs2114/tiletraveler/TileTravelerScreenTest.java
+++ src/cs2114/tiletraveler/TileTravelerScreenTest.java
@@ -1,5 +1,7 @@
 package cs2114.tiletraveler;
 
+import android.view.KeyEvent;
+import android.app.Instrumentation.ActivityMonitor;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
@@ -48,55 +50,96 @@ public class TileTravelerScreenTest
      */
     public void testStage1Clicked()
     {
-        Activity activity = getActivity();
-        final View view1 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage1);
-        final View view2 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage2);
-        final View view3 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage3);
-        final View view4 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage4);
-        final View view5 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage5);
-        final View view6 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage6);
-        final View view7 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage7);
-        // final View view8 =
-        // activity.findViewById(cs2114.tiletraveler.R.id.stage8);
-        activity.runOnUiThread(new Runnable() {
+// Activity activity = getActivity();
+// final View view1 =
+// activity.findViewById(cs2114.tiletraveler.R.id.stage1);
+// final View view2 =
+// activity.findViewById(cs2114.tiletraveler.R.id.stage2);
+// final View view3 =
+// activity.findViewById(cs2114.tiletraveler.R.id.stage3);
+// final View view4 =
+// activity.findViewById(cs2114.tiletraveler.R.id.stage4);
+// final View view5 =
+// activity.findViewById(cs2114.tiletraveler.R.id.stage5);
+// final View view6 =
+// activity.findViewById(cs2114.tiletraveler.R.id.stage6);
+// final View view7 =
+// activity.findViewById(cs2114.tiletraveler.R.id.stage7);
+// // final View view8 =
+// // activity.findViewById(cs2114.tiletraveler.R.id.stage8);
+// activity.runOnUiThread(new Runnable() {
+//
+// @Override
+// public void run()
+// {
+// view1.requestFocus();
+// view1.callOnClick();
+// assertTrue(getScreen().getCurrentStage() instanceof Stage1);
+// view2.requestFocus();
+// view2.callOnClick();
+// assertTrue(getScreen().getCurrentStage() instanceof Stage2);
+// view3.requestFocus();
+// view3.callOnClick();
+// assertTrue(getScreen().getCurrentStage() instanceof Stage3);
+// view4.requestFocus();
+// view4.callOnClick();
+// assertTrue(getScreen().getCurrentStage() instanceof Stage4);
+// view5.requestFocus();
+// view5.callOnClick();
+// assertTrue(getScreen().getCurrentStage() instanceof Stage5);
+// view6.requestFocus();
+// view6.callOnClick();
+// assertTrue(getScreen().getCurrentStage() instanceof Stage6);
+// view7.requestFocus();
+// view7.callOnClick();
+// assertTrue(getScreen().getCurrentStage() instanceof Stage7);
+// // view8.requestFocus();
+// // view8.callOnClick();
+// // assertTrue(getScreen().getCurrentStage() instanceof Stage8);
+//
+// }
+// });
+        TileTravelerScreen activity = getActivity();
 
-            @Override
-            public void run()
-            {
-                view1.requestFocus();
-                view1.callOnClick();
-                assertTrue(getScreen().getCurrentStage() instanceof Stage1);
-                view2.requestFocus();
-                view2.callOnClick();
-                assertTrue(getScreen().getCurrentStage() instanceof Stage2);
-                view3.requestFocus();
-                view3.callOnClick();
-                assertTrue(getScreen().getCurrentStage() instanceof Stage3);
-                view4.requestFocus();
-                view4.callOnClick();
-                assertTrue(getScreen().getCurrentStage() instanceof Stage4);
-                view5.requestFocus();
-                view5.callOnClick();
-                assertTrue(getScreen().getCurrentStage() instanceof Stage5);
-                view6.requestFocus();
-                view6.callOnClick();
-                assertTrue(getScreen().getCurrentStage() instanceof Stage6);
-                view7.requestFocus();
-                view7.callOnClick();
-                assertTrue(getScreen().getCurrentStage() instanceof Stage7);
-                // view8.requestFocus();
-                // view8.callOnClick();
-                // assertTrue(getScreen().getCurrentStage() instanceof Stage8);
+        getInstrumentation().invokeMenuActionSync(
+            activity,
+            cs2114.tiletraveler.R.id.stage1,
+            0);
+        assertTrue(getScreen().getCurrentStage() instanceof Stage1);
+        getInstrumentation().invokeMenuActionSync(
+            activity,
+            cs2114.tiletraveler.R.id.stage2,
+            0);
+        assertTrue(getScreen().getCurrentStage() instanceof Stage2);
+        getInstrumentation().invokeMenuActionSync(
+            activity,
+            cs2114.tiletraveler.R.id.stage3,
+            0);
+        assertTrue(getScreen().getCurrentStage() instanceof Stage3);
+        getInstrumentation().invokeMenuActionSync(
+            activity,
+            cs2114.tiletraveler.R.id.stage4,
+            0);
+        assertTrue(getScreen().getCurrentStage() instanceof Stage4);
+        getInstrumentation().invokeMenuActionSync(
+            activity,
+            cs2114.tiletraveler.R.id.stage5,
+            0);
+        assertTrue(getScreen().getCurrentStage() instanceof Stage5);
+        getInstrumentation().invokeMenuActionSync(
+            activity,
+            cs2114.tiletraveler.R.id.stage6,
+            0);
+        assertTrue(getScreen().getCurrentStage() instanceof Stage6);
+        getInstrumentation().invokeMenuActionSync(
+            activity,
+            cs2114.tiletraveler.R.id.stage7,
+            0);
+        assertTrue(getScreen().getCurrentStage() instanceof Stage7);
+// getInstrumentation().invokeMenuActionSync(activity,
+// cs2114.tiletraveler.R.id.stage8, 0);
+// assertTrue(getScreen().getCurrentStage() instanceof Stage8);
 
-            }
-        });
     }
 
 
