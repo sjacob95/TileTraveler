2c37041f6991585dc575e7f007c8119945c60805
mrzchuck <MrZchuck@Ezra-Richardss-MacBook-Pro-4.local>
2013-12-06 22:48:02 -0500

    Combined all button tests into one method.

--- src/cs2114/tiletraveler/TileTravelerScreenTest.java
+++ src/cs2114/tiletraveler/TileTravelerScreenTest.java
@@ -51,6 +51,20 @@ public class TileTravelerScreenTest
         Activity activity = getActivity();
         final View view1 =
             activity.findViewById(cs2114.tiletraveler.R.id.stage1);
+        final View view2 =
+            activity.findViewById(cs2114.tiletraveler.R.id.stage2);
+        final View view3 =
+            activity.findViewById(cs2114.tiletraveler.R.id.stage3);
+        final View view4 =
+            activity.findViewById(cs2114.tiletraveler.R.id.stage4);
+        final View view5 =
+            activity.findViewById(cs2114.tiletraveler.R.id.stage5);
+        final View view6 =
+            activity.findViewById(cs2114.tiletraveler.R.id.stage6);
+        final View view7 =
+            activity.findViewById(cs2114.tiletraveler.R.id.stage7);
+        // final View view8 =
+        // activity.findViewById(cs2114.tiletraveler.R.id.stage8);
         activity.runOnUiThread(new Runnable() {
 
             @Override
@@ -58,96 +72,31 @@ public class TileTravelerScreenTest
             {
                 view1.requestFocus();
                 view1.callOnClick();
-            }
-        });
-        assertTrue(getScreen().getCurrentStage() instanceof Stage1);
-        final View view2 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage2);
-        activity.runOnUiThread(new Runnable() {
-
-            @Override
-            public void run()
-            {
+                assertTrue(getScreen().getCurrentStage() instanceof Stage1);
                 view2.requestFocus();
                 view2.callOnClick();
                 assertTrue(getScreen().getCurrentStage() instanceof Stage2);
-            }
-        });
-
-        final View view3 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage3);
-        activity.runOnUiThread(new Runnable() {
-
-            @Override
-            public void run()
-            {
                 view3.requestFocus();
                 view3.callOnClick();
                 assertTrue(getScreen().getCurrentStage() instanceof Stage3);
-            }
-        });
-
-        final View view4 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage4);
-        activity.runOnUiThread(new Runnable() {
-
-            @Override
-            public void run()
-            {
                 view4.requestFocus();
                 view4.callOnClick();
                 assertTrue(getScreen().getCurrentStage() instanceof Stage4);
-            }
-        });
-        final View view5 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage5);
-        activity.runOnUiThread(new Runnable() {
-
-            @Override
-            public void run()
-            {
                 view5.requestFocus();
                 view5.callOnClick();
                 assertTrue(getScreen().getCurrentStage() instanceof Stage5);
-            }
-        });
-        final View view6 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage6);
-        activity.runOnUiThread(new Runnable() {
-
-            @Override
-            public void run()
-            {
                 view6.requestFocus();
                 view6.callOnClick();
                 assertTrue(getScreen().getCurrentStage() instanceof Stage6);
-            }
-        });
-        final View view7 =
-            activity.findViewById(cs2114.tiletraveler.R.id.stage7);
-        activity.runOnUiThread(new Runnable() {
-
-            @Override
-            public void run()
-            {
                 view7.requestFocus();
                 view7.callOnClick();
                 assertTrue(getScreen().getCurrentStage() instanceof Stage7);
+                // view8.requestFocus();
+                // view8.callOnClick();
+                // assertTrue(getScreen().getCurrentStage() instanceof Stage8);
+
             }
         });
-// final View view8 =
-// activity.findViewById(cs2114.tiletraveler.R.id.stage8);
-// activity.runOnUiThread(new Runnable() {
-//
-// @Override
-// public void run()
-// {
-// view4.requestFocus();
-// view4.callOnClick();
-// assertTrue(getScreen().getCurrentStage() instanceof Stage8);
-// }
-// });
-
     }
 
 
